58b97fc99fcec3909954dadeaefbc50eee8957f8
Mufti129 <ukhtytrader01@gmail.com>
2026-04-18 17:42:28 +0700

    save.py

---
+++ save.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import datetime
+
+def save_to_csv(news, trends):
+    today = datetime.date.today()
+
+    df = pd.DataFrame({
+        "date": [today],
+        "news": [", ".join(news)],
+        "trends": [", ".join(trends)]
+    })
+
+    try:
+        old = pd.read_csv("data/history.csv")
+        df = pd.concat([old, df])
+    except:
+        pass
+
+    df.to_csv("data/history.csv", index=False)
